7194a45c0513542ce55febba92f82d334f075f19
Aryan Ahuja <asafebruary@gmail.com>
2022-10-19 21:17:00 +0530

    Add files via upload

---
+++ Part 1/Team-6-Project/src/tasks/task-3-model-deployment/main.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+from application.config import Config
+
+app = Flask(__name__, template_folder = "templates")
+app.config.from_object(Config)
+app.app_context().push()
+ALLOWED_EXTENSIONS = {'jpg', 'png', 'jpg', 'jpeg'}
+os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+if __name__ == "__main__":
+    from application.controllers import *
+    app.run(port = 4555)
